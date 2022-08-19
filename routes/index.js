7ad976dc55a908dfdd3703e3b58656cd38f24553
Jenndoza <j.mendo1997@gmail.com>
2022-08-19 13:49:56 -0500

    Testing book model and communication with the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Book = require("../models").Book;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async(req, res, next) => {
+  //res.render('index', { title: 'Express' });
+  const books = await Book.findAll();
+  console.log(books);
+  res.json(books);
 });
 
 module.exports = router;
